364bacbe1ba231a2cccab9763ad1119db9b2c431
Dylan Butzlaff <500146113@fvtc.edu>
2024-02-06 12:11:23 -0600

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,10 @@
-alert("Test")
+//alert("Test")
+const hours = new Date().getHours() // get the current hour
+
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+const isEvening = hours >= 17 || hours < 4 // is it evening?
+const timeOfDay = document.querySelector('#welcome')
+if(isMorning) timeOfDay.textContent = 'Good Morning!'
+if(isAfternoon) timeOfDay.textContent = 'Good Afternoon!'
+if(isEvening) timeOfDay.textContent = 'Good Evening!'
